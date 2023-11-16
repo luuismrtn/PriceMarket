86b63f4fb555a53cee457315ed8789b683130751
luism <unconfigured@null.spigotmc.org>
2023-11-16 01:54:26 +0100

    Editar hecho

--- lib/screens/principal.dart
+++ lib/screens/principal.dart
@@ -21,13 +21,13 @@ class _MyScreenState extends State<Principal> {
     Producto(
       id: 2,
       nombre: 'Pepsi',
-      precios: [1.5, 2.0],
+      precios: [1.6, 1.0],
       categoria: 'Bebidas',
     ),
     Producto(
       id: 3,
       nombre: 'Fanta',
-      precios: [1.5, 2.0],
+      precios: [1.4, 0.2],
       categoria: 'Bebidas',
     ),
     Producto(
@@ -39,37 +39,13 @@ class _MyScreenState extends State<Principal> {
     Producto(
       id: 5,
       nombre: 'Agua',
-      precios: [1.5, 2.0],
+      precios: [1.6, 2.3],
       categoria: 'Bebidas',
     ),
     Producto(
       id: 6,
       nombre: 'Leche',
-      precios: [1.5, 2.0],
-      categoria: 'Alimentación',
-    ),
-    Producto(
-      id: 7,
-      nombre: 'Pan',
-      precios: [1.5, 2.0],
-      categoria: 'Alimentación',
-    ),
-    Producto(
-      id: 8,
-      nombre: 'Huevos',
-      precios: [1.5, 2.0],
-      categoria: 'Alimentación',
-    ),
-    Producto(
-      id: 9,
-      nombre: 'Carne',
-      precios: [1.5, 2.0],
-      categoria: 'Alimentación',
-    ),
-    Producto(
-      id: 10,
-      nombre: 'Pescado',
-      precios: [1.5, 2.0],
+      precios: [132, 28],
       categoria: 'Alimentación',
     ),
   ];
@@ -109,6 +85,77 @@ class _MyScreenState extends State<Principal> {
       ),
     );
   }
+
+  void _editarProducto(int index) async {
+  final TextEditingController _nombreController =
+      TextEditingController(text: productos[index].nombre);
+  final TextEditingController _precio1Controller =
+      TextEditingController(text: productos[index].precios[0].toString());
+  final TextEditingController _precio2Controller =
+      TextEditingController(text: productos[index].precios[1].toString());
+  final TextEditingController _categoriaController =
+      TextEditingController(text: productos[index].categoria);
+
+  await showDialog(
+    context: context,
+    builder: (BuildContext context) {
+      return AlertDialog(
+        title: Text('Editar Producto'),
+        titlePadding: EdgeInsets.fromLTRB(20, 20, 20, 5),
+        content: Column(
+          mainAxisSize: MainAxisSize.min,
+          children: [
+            TextField(
+              controller: _nombreController,
+              decoration: InputDecoration(labelText: 'Nombre del Producto'),
+            ),
+            SizedBox(height: 8),
+            TextField(
+              controller: _precio1Controller,
+              keyboardType: TextInputType.number,
+              decoration: InputDecoration(labelText: 'Precio 1'),
+            ),
+            SizedBox(height: 8),
+            TextField(
+              controller: _precio2Controller,
+              keyboardType: TextInputType.number,
+              decoration: InputDecoration(labelText: 'Precio 2'),
+            ),
+            SizedBox(height: 8),
+            TextField(
+              controller: _categoriaController,
+              decoration: InputDecoration(labelText: 'Categoría'),
+            ),
+          ],
+        ),
+        contentPadding: EdgeInsets.fromLTRB(20, 5, 20, 20),
+        actions: [
+          TextButton(
+            onPressed: () {
+              Navigator.pop(context);
+            },
+            child: Text('Cancelar'),
+          ),
+          TextButton(
+            onPressed: () {
+              setState(() {
+                productos[index].nombre = _nombreController.text;
+                productos[index].precios[0] =
+                    double.tryParse(_precio1Controller.text) ?? 0.0;
+                productos[index].precios[1] =
+                    double.tryParse(_precio2Controller.text) ?? 0.0;
+                productos[index].categoria = _categoriaController.text;
+              });
+              Navigator.pop(context);
+            },
+            child: Text('Guardar'),
+          ),
+        ],
+      );
+    },
+  );
+}
+
 
   @override
   Widget build(BuildContext context) {
@@ -190,32 +237,70 @@ class _MyScreenState extends State<Principal> {
               ],
             ),
           ),
+          Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Expanded(
+                  child: Text(
+                    'Nombre',
+                    style: TextStyle(fontWeight: FontWeight.bold),
+                    textAlign: TextAlign.center,
+                  ),
+                ),
+                Expanded(
+                  child: Text(
+                    'Mercadona',
+                    style: TextStyle(fontWeight: FontWeight.bold),
+                    textAlign: TextAlign.center,
+                  ),
+                ),
+                Expanded(
+                  child: Text(
+                    'Lidl',
+                    style: TextStyle(fontWeight: FontWeight.bold),
+                    textAlign: TextAlign.center,
+                  ),
+                ),
+              ],
+            ),
+          ),
           Expanded(
             child: ListView.builder(
               itemCount: productosFiltrados.length,
               itemBuilder: (context, index) {
                 return ListTile(
-                  title: Row(
-                    children: [
-                      Expanded(
-                        child: Text(
-                          productosFiltrados[index].nombre,
-                          style: TextStyle(fontWeight: FontWeight.bold),
+                  title: InkWell(
+                    onTap: () {
+                      _editarProducto(index);
+                    },
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        Expanded(
+                          child: Text(
+                            productosFiltrados[index].nombre,
+                            style: TextStyle(fontWeight: FontWeight.bold),
+                            textAlign: TextAlign.center,
+                          ),
                         ),
-                      ),
-                      Expanded(
-                        child: Text(
-                          'Precio 1: \$${productosFiltrados[index].precios[0]}',
-                          style: TextStyle(color: Colors.green),
+                        Expanded(
+                          child: Text(
+                            '${productosFiltrados[index].precios[0]}€',
+                            style: TextStyle(color: Colors.green),
+                            textAlign: TextAlign.center,
+                          ),
                         ),
-                      ),
-                      Expanded(
-                        child: Text(
-                          'Precio 2: \$${productosFiltrados[index].precios[1]}',
-                          style: TextStyle(color: Colors.blue),
+                        Expanded(
+                          child: Text(
+                            '${productosFiltrados[index].precios[1]}€',
+                            style: TextStyle(color: Colors.blue),
+                            textAlign: TextAlign.center,
+                          ),
                         ),
-                      ),
-                    ],
+                      ],
+                    ),
                   ),
                 );
               },
